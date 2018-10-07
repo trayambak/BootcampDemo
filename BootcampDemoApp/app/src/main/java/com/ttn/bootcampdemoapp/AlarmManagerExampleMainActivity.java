689f2c1442544ba8e56925582fb229c1cad700a0
trayambak.mishra@tothenew.com <trayambak.mishra@tothenew.com>
2018-10-07 21:34:14 +0530

    initial commit with alaram manager layout

---
+++ BootcampDemoApp/app/src/main/java/com/ttn/bootcampdemoapp/AlarmManagerExampleMainActivity.java
@@ -0,0 +1,13 @@
+package com.ttn.bootcampdemoapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AlarmManagerExampleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_manager_example_main);
+    }
+}
